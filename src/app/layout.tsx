dc20fcc213beca80e955acdbd15fd257b32244fd
unknown <ariyeric@gmail.com>
2025-02-28 15:56:52 +0100

    add authentication page and theme provider; implement theme toggler and UI components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -32,18 +32,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${WorkSans.className} `}>
-        <main className="">
-          <Navbar />
-          <div className="flex">
-            <div className="hidden md:block h-[100vh] w-[300px]">
-              <Sidebar />
-            </div>
-            <div className="p-5 w-full md:max-w-[1140px]">
+      <ThemeProvider
+          attribute='class'
+          defaultTheme='light'
+          enableSystem={true}
+          storageKey='dashboard-theme'
+        >
           {children}
-            </div>
-          </div>
-        </main>
-      </body>    
+          <Toaster />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,41 +3,45 @@ import Link from "next/link";
 import logo from "../components/images/futuresampLogo.png"
 import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import ThemeToggler from "@/components/ThemeToggler";
 
 const Navbar = () => {
     return ( 
         <>
-           <nav className="bg-primary dark:bg-slate-700 py-2 px-5 flex justify-between">
+            <nav className="bg-primary dark:bg-slate-700 py-2 px-5 flex justify-between">
                 <Link href="/">
-                <Image src={logo} alt="ericPaneHome" width={40} />
+                    <Image src={logo} alt="ericPaneHome" width={40} />
                 </Link>
-                
-                <DropdownMenu>
-                    <DropdownMenuTrigger>
-                        <Avatar>
-                            <AvatarImage src="https://ui.shadcn.com/avatars/shadcn.jpg" />
-                            <AvatarFallback className="text-neutral-900">
-                                OE
-                            </AvatarFallback>
-                        </Avatar>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>
-                            <Link href="/profile">
-                                Profile
-                            </Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem>
-                            <Link href="/auth">
-                                Logout
-                            </Link>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
 
-            </nav> 
+                <div className="flex items-center">
+                    <ThemeToggler />
+
+                    <DropdownMenu>
+                        <DropdownMenuTrigger>
+                            <Avatar>
+                                <AvatarImage src="https://ui.shadcn.com/avatars/shadcn.jpg" />
+                                <AvatarFallback className="text-neutral-900">
+                                    OE
+                                </AvatarFallback>
+                            </Avatar>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>
+                                <Link href="/profile">
+                                    Profile
+                                </Link>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Link href="/auth">
+                                    Logout
+                                </Link>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+            </nav>
         </>
      );
 }
